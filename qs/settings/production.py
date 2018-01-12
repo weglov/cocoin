4d824b565abe433d32b5fd424ceb4c0c2ff5bca4
Viktor Shcheglov <vscheglov@odin.com>
2018-01-12 14:53:55 +0300

    postgress uri new

--- qs/settings/production.py
+++ qs/settings/production.py
@@ -6,5 +6,5 @@ ENVIRONMENT = 'production'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 DATABASES['default'] = dj_database_url.config(
-    default='postgres://jmhwrltxbineri:d18d3b3e3342410e07312c7f124b7ddc06be69037c71b49e9fc4e584cd445f70@ec2-54-217-218-80.eu-west-1.compute.amazonaws.com:5432/d45t5k25g6o549'
+    default='postgres://mzobacrcbcbfzz:0355b0d7b00b0f16818714bbc8f3f2f92026e4cd5fbc979eab7ea7da31c1dd30@ec2-54-217-218-80.eu-west-1.compute.amazonaws.com:5432/ddadu6455cjs4u'
 )
