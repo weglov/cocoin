4378750db11c2c3827756c5dab4ed1f4137088ca
Viktor Shcheglov <vscheglov@odin.com>
2018-01-12 12:06:48 +0300

    fix hreoku deploy

--- qs/settings.py
+++ qs/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +144,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
