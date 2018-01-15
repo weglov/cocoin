4b003826bd5caa5d3fb104cd84b5895ca9f84e8f
Viktor Shcheglov <vscheglov@odin.com>
2018-01-15 16:25:31 +0300

    add logo path

--- app/grab.py
+++ app/grab.py
@@ -12,9 +12,9 @@ def get_coins(url=URL, *params):
 	r = requests.get(url, params=params)
 	return r.json()
 
-def update_coin_list():
+def update_coin_list(with_logo=False):
 	for coin in get_coins():
-		# try:
+		try:
 			item = Coin.objects.update_or_create(
 				name=coin['name'],
 				defaults={
@@ -23,21 +23,21 @@ def update_coin_list():
 					'supply': coin['total_supply'],
 					'volume_24h': coin['24h_volume_usd'],
 					'market_cap_usd': coin['market_cap_usd'],
-					'logo': '{}.png'.format(coin['id'])
+					'logo': '/coins/{}.png'.format(coin['id'])
 				},
 			)
 
-			if random.randint(1,25) == 10:
+			if with_logo:
 				load_logo(coin['id'])
 
 			CoinShot.objects.create(coin=item[0], value=item[0].price)
-		# except:
-		# 	print('Error save: {} ({})- {}'.format(coin['name'], coin['symbol'], coin['price_usd']))
+		except:
+			print('Error save: {} ({})- {}'.format(coin['name'], coin['symbol'], coin['price_usd']))
 
 
 def load_logo(id):
 	r = requests.get(IMAGE_URL + id + '.png', stream=True)
 	if r.status_code == 200:
-		with open('uploads/' + id + '.png', 'wb') as out_file:
+		with open('qs/uploads/coins/' + id + '.png', 'wb') as out_file:
 			shutil.copyfileobj(r.raw, out_file)
 	del r
